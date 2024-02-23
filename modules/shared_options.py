34f72eb0c496c3aa69213eddecd4e8930ba15e2f
Junjie Bai <jbai@lepton.ai>
2024-02-23 00:33:20 +0000

    disable mmap loading

--- modules/shared_options.py
+++ modules/shared_options.py
@@ -116,7 +116,7 @@ options_templates.update(options_section(('system', "System", "system"), {
     "multiple_tqdm": OptionInfo(True, "Add a second progress bar to the console that shows progress for an entire job."),
     "print_hypernet_extra": OptionInfo(False, "Print extra hypernetwork information to console."),
     "list_hidden_files": OptionInfo(True, "Load models/files in hidden directories").info("directory is hidden if its name starts with \".\""),
-    "disable_mmap_load_safetensors": OptionInfo(False, "Disable memmapping for loading .safetensors files.").info("fixes very slow loading speed in some cases"),
+    "disable_mmap_load_safetensors": OptionInfo(True, "Disable memmapping for loading .safetensors files.").info("fixes very slow loading speed in some cases"),
     "hide_ldm_prints": OptionInfo(True, "Prevent Stability-AI's ldm/sgm modules from printing noise to console."),
     "dump_stacks_on_signal": OptionInfo(False, "Print stack traces before exiting the program with ctrl+c."),
 }))
